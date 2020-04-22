ae2e1f8e95bb7b512f17aaec2062317d788aef93
Raghu Mulukutla <raghu.tjm@gmail.com>
2020-04-22 13:32:52 -0400

    tried to fix slope divide by zero errors with interpolation

---
+++ accelerometer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import datetime
+import numpy as np
+import pytz
+import scipy
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from numpy import linalg as LA
+import time
+from matplotlib import style
+from scipy import signal
+from scipy import fftpack
+import pywt
